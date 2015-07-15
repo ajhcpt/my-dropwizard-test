050880c914de7c67c0a029331d6b295cc098677d
andrewhe <andrew.henderson@allangray.co.za>
2015-07-15 18:10:20 +0200

    Angular start

---
+++ src/main/resources/app.js
@@ -0,0 +1,24 @@
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function() {
+        this.product = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Another gem',
+            price: 5.95,
+            description: '. . .',
+            canPurchase: false,
+            soldOut: false
+        }
+    ];
+})();
