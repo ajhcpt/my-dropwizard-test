666c35f2c3b58041966108a82dc017837342f495
andrewhe <andrew.henderson@allangray.co.za>
2015-07-15 11:46:46 +0200

    Dont need object mapper code with custom saying serializer. can just return the saying object.

--- src/main/scala/com/example/helloworld/resources/HelloWorldResource.scala
+++ src/main/scala/com/example/helloworld/resources/HelloWorldResource.scala
@@ -1,24 +1,23 @@
 package com.example.helloworld.resources
 
 import java.util.concurrent.atomic.AtomicLong
-import javax.ws.rs.core.{Response, MediaType}
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.{GET, Path, Produces, QueryParam}
-
 import com.codahale.metrics.annotation.Timed
 import com.example.helloworld.core.Saying
-import com.fasterxml.jackson.databind.ObjectMapper
 
 @Path("/hello-world")
 @Produces(Array(MediaType.APPLICATION_JSON))
 class HelloWorldResource(template: String, defaultName: String) {
   var counter: AtomicLong = _
+
   @GET
   @Timed
-  def sayHello(@QueryParam("name") name: String): String = {
+  def sayHello(@QueryParam("name") name: String): Saying = {
     val value = String.format(template, Option(name) match {
       case Some(x) => name
       case None => defaultName
     })
-    new ObjectMapper().writeValueAsString(new Saying(1, value))
+    new Saying(1, value)
   }
 }
